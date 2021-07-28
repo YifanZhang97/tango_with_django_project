073600afa56f3173b759a75c37cbd6cace8ff9be
YifanZhang97 <2594230Z@student.gla.ac.uk>
2021-07-29 02:45:36 +0800

    ch6

--- populate_rango.py
+++ populate_rango.py
@@ -38,9 +38,9 @@ def populate():
     # and then adds all the associated pages for that category.
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat, cat_data['views'], cat_data['likes'])
+        c = add_cat(cat, views=cat_data['views'], likes=cat_data['likes'])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'], p['views'])
+            add_page(c, p['title'], p['url'], views=p['views'])
 
     # Print out the categories we have added.
     for c in Category.objects.all():
