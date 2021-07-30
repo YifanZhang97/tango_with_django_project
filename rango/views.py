e8e033ccbff4817d43d4bba75d884ed32cc11a14
YifanZhang97 <2594230Z@student.gla.ac.uk>
2021-07-30 17:43:39 +0800

    ch9

--- rango/views.py
+++ rango/views.py
@@ -7,6 +7,7 @@ from rango.models import Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.shortcuts import redirect
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     # Query the database for a list of ALL categories currently stored.
@@ -214,3 +215,8 @@ def user_login(request):
         # No context variables to pass to the template system, hence the
         # blank dictionary object...
         return render(request, 'rango/login.html')
+
+
+@login_required
+def restricted(request):
+    return HttpResponse("Since you're logged in, you can see this text!")
