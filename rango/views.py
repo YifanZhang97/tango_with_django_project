2efad382ad4db3b72f7df88ab93c82d840aef87a
YifanZhang97 <2594230Z@student.gla.ac.uk>
2021-07-30 18:51:39 +0800

    ch10

--- rango/views.py
+++ rango/views.py
@@ -23,10 +23,17 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
 
+    request.session.set_test_cookie()
+
     # Render the response and send it back!
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
+
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
+        
     return render(request, 'rango/about.html')
 
 def show_category(request, category_name_slug):
